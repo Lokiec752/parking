75e71cc4c5168af190241340eda9f71fbdb829c8
Marcin Gradzki <marcin.gradzki.dev@gmail.com>
2024-11-10 23:11:09 +0100

    Remove dotenv from the project

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,8 +1,6 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
-import "dotenv/config";
-
 const pool = new Pool({
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
